9da583aaf277b3654f999382d6ba331b86414a68
Joseph Woo <jwoo@Josephs-Air.home>
2016-12-22 09:09:14 -0500

    adding files to github

---
+++ currencyConverter.js
@@ -0,0 +1,18 @@
+angular.module('converter',[])
+.controller('converterCtrl', ['$scope', '$http', function($scope,$http){
+	var selectCtrl = this;
+	$scope.rates = {};
+	$http.get('http://api.fixer.io/latest?base=ZAR')
+	.then(function(res){
+		$scope.rates = res.data.rates;
+		$scope.toType = $scpoe.rates.EUR;
+		$scope.fromType = $scope.rates.USD;
+		$scope.fromValue = 1;
+		$scope.convertFunction();
+	});
+	$scope.convertFunction = function(){
+		$scope.toValue = $scope.fromValue * ($scope.toType * (1 / $scope.fromType));
+		$scope.toValue = $scope.toValue;
+	};
+}]);
+
